ba8e06f89d0d1bdb76f8a8d9c71afdec7d3db3c1
C <caaarloos300@gmail.com>
2023-02-11 04:07:18 +0900

    cleaning

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -40,14 +40,6 @@ export const HomeView: FC = ({ }) => {
           <p>Unleash the full power of blockchain with Solana and Next.js 13.</p>
           <p className='text-slate-500 text-2x1 leading-relaxed'>Full-stack Solana applications made easy.</p>
         </h4>
-        <div className="relative group">
-          <div className="absolute -inset-0.5 bg-gradient-to-r from-indigo-500 to-indigo-500 rounded-lg blur opacity-40 animate-tilt"></div>
-          <div className="max-w-md mx-auto mockup-code bg-primary border-2 border-[#5252529f] p-6 px-10 my-2">
-            <pre data-prefix=">">
-              <code className="truncate">{`npx create-solana-dapp <dapp-name>`} </code>
-            </pre>
-          </div>
-        </div>
         <div className="flex flex-col mt-2">
           <RequestAirdrop />
           <h4 className="md:w-full text-2xl text-slate-300 my-2">
