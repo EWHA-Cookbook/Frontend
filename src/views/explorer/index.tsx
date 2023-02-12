357236de7ff526db4a0bfe0c4fa0c029122851b4
mj <mjk4534@ewhain.net>
2023-02-13 02:20:47 +0900

    [FEAT] Main

--- src/views/explorer/index.tsx
+++ src/views/explorer/index.tsx
@@ -1,21 +1,17 @@
-
-import { FC } from "react";
+import { FC } from 'react';
 import { SignMessage } from '../../components/SignMessage';
 import { SendTransaction } from '../../components/SendTransaction';
 import { SendVersionedTransaction } from '../../components/SendVersionedTransaction';
 
-export const ExplorerView: FC = ({ }) => {
-
+export const ExplorerView: FC = ({}) => {
   return (
     <div className="md:hero mx-auto p-4">
       <div className="md:hero-content flex flex-col">
         <h1 className="text-center text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-indigo-500 to-fuchsia-500 mt-10 mb-8">
-          Explorer
+          Cookbook
         </h1>
         {/* CONTENT GOES HERE */}
-        <div className="text-center">
-          Hello
-        </div>
+        <div className="text-center">Hello</div>
       </div>
     </div>
   );
