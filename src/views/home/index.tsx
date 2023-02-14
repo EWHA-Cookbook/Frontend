80bfaba75372524bd476443a9c7da069ee80bb6d
mj <mjk4534@ewhain.net>
2023-02-15 05:19:17 +0900

    [Feat] Main Page

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -12,48 +12,46 @@ import pkg from '../../../package.json';
 // Store
 import useUserSOLBalanceStore from '../../stores/useUserSOLBalanceStore';
 
-export const HomeView: FC = ({ }) => {
+// image
+import Image from 'next/image';
+
+export const HomeView: FC = ({}) => {
   const wallet = useWallet();
   const { connection } = useConnection();
 
-  const balance = useUserSOLBalanceStore((s) => s.balance)
-  const { getUserSOLBalance } = useUserSOLBalanceStore()
+  const balance = useUserSOLBalanceStore(s => s.balance);
+  const { getUserSOLBalance } = useUserSOLBalanceStore();
 
   useEffect(() => {
     if (wallet.publicKey) {
-      console.log(wallet.publicKey.toBase58())
-      getUserSOLBalance(wallet.publicKey, connection)
+      console.log(wallet.publicKey.toBase58());
+      getUserSOLBalance(wallet.publicKey, connection);
     }
-  }, [wallet.publicKey, connection, getUserSOLBalance])
+  }, [wallet.publicKey, connection, getUserSOLBalance]);
 
   return (
-
-    <div className="md:hero mx-auto p-4">
+    <div className="md:hero p-4">
+      <svg className="">
+        <ellipse></ellipse>
+      </svg>
       <div className="md:hero-content flex flex-col">
-        <div className='mt-6'>
-        <div className='text-sm font-normal align-bottom text-right text-slate-600 mt-4'>v{pkg.version}</div>
-        <h1 className="text-center text-5xl md:pl-12 font-bold text-transparent bg-clip-text bg-gradient-to-br from-indigo-500 to-fuchsia-500 mb-4">
-          Solana Next
-        </h1>
+        <div className="flex flex-col flex-start mt-6 left-0 top-0 ">
+          <div className="text-sm font-normal align-bottom text-right text-slate-600 mt-4">v{pkg.version}</div>
+          <Image className="inline" src="/Logo.png" alt="cookbook icon" width={391} height={70} />
+          <div className="w-38 h-1.5 border-t-2 border-b-1 border-black"></div>
+          <h1 className="font-serif text-left text-5xl md:pl-12 font-bold text-transparent text-inherit">
+            Solana Cookbook
+          </h1>
         </div>
-        <h4 className="md:w-full text-2x1 md:text-4xl text-center text-slate-300 my-2">
-          <p>Unleash the full power of blockchain with Solana and Next.js 13.</p>
-          <p className='text-slate-500 text-2x1 leading-relaxed'>Full-stack Solana applications made easy.</p>
+        <h4 className="mt-52 md:w-full text-2x1 md:text-4xl text-center text-slate-300 my-2">
+          <p className="font-serif font-normal text-black">
+            {' '}
+            See more <br /> Dishes
+          </p>
+          <p>Arrow</p>
         </h4>
-        <div className="flex flex-col mt-2">
-          <RequestAirdrop />
-          <h4 className="md:w-full text-2xl text-slate-300 my-2">
-          {wallet &&
-          <div className="flex flex-row justify-center">
-            <div>
-              {(balance || 0).toLocaleString()}
-              </div>
-              <div className='text-slate-600 ml-2'>
-                SOL
-              </div>
-          </div>
-          }
-          </h4>
+        <div className="flex flex-col mt-20 border-1 border-black py-42.5">
+          <div className=""></div>
         </div>
       </div>
     </div>
