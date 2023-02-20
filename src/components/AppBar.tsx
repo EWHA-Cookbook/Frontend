8612caad0b7e232c45eb0a2b9b77df1235b20926
Chaerin Kim <decentralized0212@gmail.com>
2023-02-20 22:13:58 +0900

    Edit Navbar
    Edit href && target of AppBar
    Edit Pages' titles

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -22,8 +22,7 @@ export const AppBar: React.FC = () => {
         <div className="navbar-start align-items-center">
           <div className="hidden sm:inline w-22 h-22 md:p-2 ml-10">
             <Link
-              href="https://solana.com"
-              target="_blank"
+              href="http://localhost:3000/"
               rel="noopener noreferrer"
               passHref
               className="text-secondary hover:text-white"
