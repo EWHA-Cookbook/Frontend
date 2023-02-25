be9c506a98dc3cc40b7703e0eb45be29d43b5ce9
mj <mjk4534@ewhain.net>
2023-02-26 02:23:19 +0900

    [Design] Main Page design

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -30,48 +30,36 @@ export const HomeView: FC = ({}) => {
   }, [wallet.publicKey, connection, getUserSOLBalance]);
 
   return (
-    
     <div className="p-4">
-   
-          
       {/* <svg className="">
         <ellipse></ellipse>
       </svg> */}
-      
+
       <div className="pt-40 px-36 flex flex-col">
-        
         <div className="flex flex-col mt-6 left-0 top-0 w-fit">
           <div className="text-sm font-normal align-bottom text-right text-slate-600 mt-4">v{pkg.version}</div>
-          
+
           <Image className="inline" src="/Logo.png" alt="cookbook icon" width={391} height={70} />
-          
+
           <div className="w-38 h-1.5 border-t-2 border-b-1 border-solid border-black"></div>
-          <h1 className="font-serif text-left text-7xl md:mx-5 font-bold text-transparent text-inherit">
-            Solana Cookbook
-          </h1>
-          
-          
+          <h1 className="font-serif text-left text-7xl md:mx-5 font-bold text-inherit">Solana Cookbook</h1>
+
           <div className="w-38 h-1.5 border-t-1 border-b-2 border-black"></div>
         </div>
-        <div>
-          
-        </div>
-        
+        <div></div>
+
         <h4 className="mt-52 md:w-full text-2xl md:text-4xl text-center text-slate-300 my-2">
-          
           <p className="font-serif font-normal text-black">
             {' '}
             See more <br /> Dishes
           </p>
           <p>↓</p>
         </h4>
-        <div className="flex grid grid-cols-3 border-1 border-black gap-0 mt-80 py-42.5">
-          <div className="col-span-1 bg-transparent">
-          <Image src="/minimal1.png" alt="cookbook icon" width={500} height={140} />
-
+        <div className="aspect-squre border-collapse border-hidden grid grid-cols-3 gap-0 mt-80 py-42.5 w-5/6">
+          <div className="grid-cols-1 border border-solid border-10 border-black col-span-1 bg-transparent">
+            <Image src="/minimal1.png" alt="cookbook icon" width={500} height={140} />
           </div>
-          <div className="col-span-2 bg-transparent">
-            
+          <div className="aspect-squre w-full col-span-2 bg-transparent border border-solid border-10 border-black ">
             <h4 className="py-36 md:w-full text-2xl md:text-4xl text-center text-slate-300 my-2">
               <p className="font-serif font-normal text-black text-center">
                 {' '}
@@ -83,7 +71,6 @@ export const HomeView: FC = ({}) => {
             </h4>
           </div>
           <div className="col-span-2 bg-transparent">
-            
             <h4 className="py-36 md:w-full text-2xl md:text-4xl text-center text-slate-300 my-2">
               <p className="font-serif font-normal text-black text-center">
                 {' '}
@@ -95,10 +82,10 @@ export const HomeView: FC = ({}) => {
             </h4>
           </div>
           <div className="col-span-1 bg-transparent">
-          <Image src="/minimal2.png" alt="cookbook icon" width={500} height={140} />
+            <Image src="/minimal2.png" alt="cookbook icon" width={500} height={140} />
           </div>
           <div className="col-span-1 bg-transparent">
-          <Image src="/minimal4.png" alt="cookbook icon" width={500} height={140} />
+            <Image src="/minimal4.png" alt="cookbook icon" width={500} height={140} />
           </div>
           <div className="col-span-2 bg-transparent">
             <h4 className="py-36 md:w-full text-2x1 md:text-4xl text-center text-slate-300 my-2">
@@ -119,7 +106,7 @@ export const HomeView: FC = ({}) => {
           <h4 className="mt-2 md:w-full text-2xl md:text-2xl font-serif font-normal text-black my-2 text-right">
             <Link href="/explorer">more </Link>
           </h4>
-          <div className="grid grid-cols-1 md:grid-cols-4 gap-x-9 h-40">
+          <div className="grid grid-cols-1 md:grid-cols-4 gap-x-9 h-40 border">
             <div className="bg-white border-black border-1 border-solid">a</div>
             <div className="bg-white border-black border-1 border-solid">b</div>
             <div className="bg-white border-black border-1 border-solid">c</div>
@@ -127,7 +114,6 @@ export const HomeView: FC = ({}) => {
           </div>
         </div>
       </div>
-  </div>
- 
+    </div>
   );
 };
